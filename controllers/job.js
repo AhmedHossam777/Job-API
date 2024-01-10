8a5a0f6cceab082063d010806c2d3858208eed5e
Ahmed Hossam <AhmedHossam9226@gmail.com>
2024-01-10 19:24:19 +0200

    create all CRUD operations controllers

--- controllers/job.js
+++ controllers/job.js
@@ -1,19 +1,35 @@
 const Job = require('../models/Job');
 const { StatusCodes } = require('http-status-codes');
+const ApiError = require('../errors/apiError');
 
 const getAllJobs = async (req, res) => {
-  res.send('All Jobs');
+  const jobs = await Job.find({ createdBy: req.user.id }).sort('createdAt'); //? we will get the all the jobs of the specific user
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    result: jobs.length,
+    jobs,
+  });
 };
 
-const getJob = async (req, res) => {
-  res.send('Single Job');
+const getJob = async (req, res, next) => {
+  const jobId = req.params.id;
+  const job = await Job.findOne({ _id: jobId, createdBy: req.user.id });
+
+  if (!job) {
+    return next(
+      ApiError.create('NOT FOUND!', StatusCodes.BAD_REQUEST)
+    );
+  }
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    job,
+  });
 };
 
 const createJob = async (req, res) => {
-  
   req.body.createdBy = req.user.id;
-  console.log(req.user);
-  
+
   const job = req.body;
 
   const newJob = await Job.create(job);
@@ -25,11 +41,26 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send('Update Job');
+  const jobId = req.params.id;
+  const data = req.body;
+
+  const newJob = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: req.user.id },
+    data
+  );
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    newJob,
+  });
 };
 
 const deleteJob = async (req, res) => {
-  res.send('Delete Job');
+  const jobId = req.params.id;
+  await Job.findOneAndDelete({ _id: jobId, createdBy: req.user.id });
+  res.status(StatusCodes.NO_CONTENT).json({
+    status: 'success',
+  });
 };
 
 module.exports = {
